2da9acd6bdd0dc863e4502690578c681a906fe17
Bryant Wolf <bryant@intrepid.io>
2016-03-25 14:22:26 -0400

    add approver to PRBot

--- app/services/ship_messenger.rb
+++ app/services/ship_messenger.rb
@@ -2,7 +2,7 @@ class ShipMessenger < SlackMessenger
   private
 
   def text
-    "<@#{pull_request.user.name}> you can #{ship} #{pull_request.link}. #{compliment} - Approved by pull_request.approver.name"
+    "<@#{pull_request.user.name}> you can #{ship} #{pull_request.link}. #{compliment} - Approved by #{pull_request.approver.name}"
   end
 
   def ship
