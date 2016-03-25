86d807db4800332babeff3665c9401171e8082f7
Bryant Wolf <bryant@intrepid.io>
2016-03-25 15:33:49 -0400

    updated help message

--- app/controllers/prbot_controller.rb
+++ app/controllers/prbot_controller.rb
@@ -5,7 +5,7 @@ class PrbotController < ApplicationController
     1. Submit a pull request                        /pr {optional description} {github link}\n
     2. View a list of open pull requests:           /prlist\n
     3. Approve a pull request for shipping:         /ship @{username} /{pr number}\n
-    4. View a list of pull requests approved today  /ship_list\n
+    4. View a list of pull requests approved today  /shiplist\n
     5. Add another channel to PRBot:                /pr_web_hook {webhook link}
     "
   end
