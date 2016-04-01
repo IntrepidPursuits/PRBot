a056dc354947cfe0281bbd5f0fc26660d64f2766
Bryant Wolf <bryant@intrepid.io>
2016-04-01 14:17:28 -0400

    pull request delete formated for slash command

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :pull_requests, only: [:index, :create]
-  resource :pull_requests, only: [:destroy]
+  post '/pull_requests/delete', to: 'delete_pull_requests#create'
   resources :ships, only: [:index,:create]
   resources :channel_web_hooks, only: [:create]
   resources :prbot, only: [:index]
